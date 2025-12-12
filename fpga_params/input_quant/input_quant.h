273654387b87e4127fa2fcc1147855408c0041e6
黄来俊（Huang Laijun） <3057923194@qq.com>
2025-12-12 12:03:17 +0800

    保存更多模型信息

---
+++ fpga_params/input_quant/input_quant.h
@@ -0,0 +1,8 @@
+// Input quantization parameters
+#ifndef INPUT_QUANT_H
+#define INPUT_QUANT_H
+
+static const float INPUT_SCALE = 0.037146877497434616f;
+static const signed char INPUT_ZP = 57;
+
+#endif
